fd85c50b5c55742416d8c28cc4f5ff642fb35e27
ManarElsaid <elsaidmanar@gmail.com>
2024-08-08 13:12:37 +0300

    login component structure

--- dashboard/src/components/forms/LogIn.js
+++ dashboard/src/components/forms/LogIn.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import usr_icon from '../../assets/user.png'
+import password_icon from '../../assets/password.png'
+import email_icon from '../../assets/email.png'
+import './form.css'
 
-export const logIn = () => {
+export const LogIn = () => {
   return (
-    <div>logIn</div>
+    <div className='container'>
+      <div>
+        <div>Create Account</div>
+      </div>
+      <div className='forms'>
+        <div className='input'>
+          <img src={password_icon} alt='' />
+          <input type='text' />
+        </div>
+        <div className='input'>
+          <img src={email_icon} alt='' />
+          <input type='email' />
+        </div>
+        <div className='input'>
+          <img src={usr_icon} alt='' />
+          <input type='password' />
+        </div>
+      </div>
+      <div className='submition-container'>
+        <div className='submit'>Sign Up</div>
+        <div className='submit'>Login</div>
+      </div>
+    </div>
   )
 }
--- dashboard/src/site/layouts/Home.js
+++ dashboard/src/site/layouts/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Nav } from '../../components/frame/Nav'
 import { Header } from '../../components/frame/Header'
 import { Footer } from '../../components/frame/Footer'
+import { LogIn } from '../../components/forms/LogIn'
 
 export const Home = () => {
   return (
     <>
     <Nav />
     <Header />
+    <LogIn />
     <Footer/>
     </>
   )
