cd1aa4119fade868d3ea6f6bf9df5027c8ef6de7
Sarahgad <sarahgad91@gmail.com>
2024-08-10 06:12:35 +0300

    chor: activat btns in card  share,next ,disocver

--- dashboard/src/components/Card.js
+++ dashboard/src/components/Card.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../../src/App.css'
 import nextIcon from '../assets/next.png';
 import returnIcon from '../assets/Return.png';
 import discoverIcon from '../assets/discover.png';
 import { useNavigate } from "react-router-dom";
+import {FacebookIcon, FacebookShareButton, TwitterIcon, TwitterShareButton, WhatsappIcon, WhatsappShareButton} from 'react-share'
 
+export const ShareCard = () => {
+  const currentUrl = 'www.google.com'/*window.location.href*/
+  return(
+    <>
+      <FacebookShareButton  url={currentUrl}>
+    <FacebookIcon className='sharebuttom H' />
+  </FacebookShareButton>
+
+  <TwitterShareButton url={currentUrl} >
+<TwitterIcon className='sharebuttom H' />
+  </TwitterShareButton>
+
+<WhatsappShareButton url={currentUrl}>
+<WhatsappIcon className='sharebuttom H' /> 
+</WhatsappShareButton>
+    </>
+  
+  )
+
+}
 export function NavigatEditButton() {
   const navigate = useNavigate(); // Hook to navigate programmatically
 
@@ -16,11 +37,18 @@ export function NavigatEditButton() {
     )
   };
 
-export const Card =({title, question, answer, click}) => {
+export const Card =({title, question, answer, nextclick, prevclick,}) => {
+  const [isvisible, setvisible] = useState(false);
+
+const Toggleanswer = ()=>{
+  setvisible(!isvisible)
+}
   return (
     <div className='Container'> 
       <div className='return'>
-      <img alt='next' src={returnIcon} width={50} height={31}/>
+        <button>
+        <img alt='next' src={returnIcon} width={50} height={31}/>
+        </button>
       </div>
       <div className='text'>
         <h3>{title}</h3>
@@ -28,22 +56,32 @@ export const Card =({title, question, answer, click}) => {
       <div className='Cardbox'>
         <div className='Question text'><h4>{question}</h4></div>
         <div className='text'>
-            <p className='inline'>Answer</p>    
+            <p className='inline'>Answer</p>
+            <button onClick={Toggleanswer}>
             <img  alt='discover' src={discoverIcon} width={19.94} height={15.97}/>
-          <div className='boxAnswer'>
+            </button>
+          <div className={isvisible ? 'visible boxAnswer' : 'hidden boxAnswer'}>
             <p>{answer}</p>
             </div>
           </div>       
           <div className='nextCard'>
-            <img alt='next' src={nextIcon} width={30} height={30}/>
+          <button onClick={prevclick}>
+            <img alt='next' src={returnIcon} width={30} height={30} />
+            </button>
+            <button onClick={nextclick}>
+            <img alt='next' src={nextIcon} width={30} height={30} />
+            </button>
+         
+            
             </div>
           </div>
           <div className='btns'>
-            <button className='share green H'>Share</button>
+            {/* <button className='share green H'>Share</button> */}
             <NavigatEditButton/>
-            <button className='delete red H' >Delete</button>
-
+            <button className='delete red ' >Delete</button>
+            <ShareCard/>
           </div>
+          
 
       </div>
   )
