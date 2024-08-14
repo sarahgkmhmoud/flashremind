689e28a44ccaf6532430e077b83d2a4a0f42618c
ManarElsaid <elsaidmanar@gmail.com>
2024-08-15 01:48:25 +0300

    handling inputs validation

--- dashboard/src/components/forms/SignUpLogIn.js
+++ dashboard/src/components/forms/SignUpLogIn.js
@@ -6,14 +6,48 @@ import "./form.css";
 
 export const SignUpLogIn = () => {
   const [authAction, setAuthAction] = useState("Create Account");
+
   const [formInputs, setFormInput] = useState({
     name: "",
     email: "",
     password: "",
   });
 
+  const [errors, setErrors] = useState({});
+
+  const validateInputs = () => {
+    const validationErrors = {};
+    const nameRegex = /^[a-zA-Z][a-zA-Z0-9-_]{3,16}$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!formInputs.name.trim()){
+      validationErrors.name = "username is required";
+    } else if(!nameRegex.test(formInputs.name)){
+      validationErrors.name = "Please enter a valid name (3-16 characters, letters and numbers only).";
+    }
+
+    if(!formInputs.email.trim()){
+      validationErrors.email = "email is required";
+    } else if(!emailRegex.test(formInputs.email)){
+      validationErrors.email = "please enter a valid email";
+    }
+
+    if(!formInputs.password.trim()){
+      validationErrors.password = "password is required";
+    } else if(formInputs.password.length < 8){
+      validationErrors.password = "Password must be at least 8 characters long.";
+    }
+
+    
+    setErrors(validationErrors);
+
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    validateInputs();
+
     if (authAction === "Create Account") {
       console.log("Signing up:", formInputs);
       // Handle signup logic here
@@ -46,6 +80,8 @@ export const SignUpLogIn = () => {
                 setFormInput({ ...formInputs, name: e.target.value });
               }}
             />
+            {errors.name && <span className='error'>{errors.name}</span>
+            }
           </div>
         )}
 
@@ -59,6 +95,7 @@ export const SignUpLogIn = () => {
               setFormInput({ ...formInputs, email: e.target.value });
             }}
           />
+          {errors.email && <span className='error'>{errors.email}</span>}
         </div>
 
         <div className="input">
@@ -71,6 +108,7 @@ export const SignUpLogIn = () => {
               setFormInput({ ...formInputs, password: e.target.value });
             }}
           />
+          {errors.password && <span className='error'>{errors.password}</span>}
         </div>
 
         <div className="submit-buttons">
